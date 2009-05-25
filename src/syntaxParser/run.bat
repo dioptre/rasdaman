33d75ff4aab0f98db56874ec16ec9a4e440aab55
Andrei Aiordachioaie <a.aiordachioaie@jacobs-university.de>
2009-05-25 10:18:29 +0200

    Removed old grammar source files

--- src/syntaxParser/run.bat
+++
@@ -1,8 +0,0 @@
-java JLex.Main wcps.lex
-java java_cup.Main -parser WCPSParser -symbols WCPSSymbol <wcps.cup 
-javac wcps.lex.java
-javac WCPSParser.java WCPSSymbol.java WCPSRequestProcessor.java
-java WCPSRequestProcessor
-
-
-
