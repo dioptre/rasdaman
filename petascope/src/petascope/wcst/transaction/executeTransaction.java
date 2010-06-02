c542ee5a7b3f678bb9b3c0388cce1a19c680c021
Constantin <jucovschi@gmail.com>
2010-06-02 21:49:04 +0200

    joined .gitignore files of petascope and rasdaman repositories. replaced petascope headers with rasdaman community headers.

--- petascope/src/petascope/wcst/transaction/executeTransaction.java
+++ petascope/src/petascope/wcst/transaction/executeTransaction.java
@@ -1,23 +1,24 @@
 /*
- * This file is part of PetaScope.
+ * This file is part of rasdaman community.
  *
- * PetaScope is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * Rasdaman community is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * PetaScope is distributed in the hope that it will be useful,
+ * Rasdaman community is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with PetaScope. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with rasdaman community.  If not, see <http://www.gnu.org/licenses/>.
  *
- * For more information please see <http://www.PetaScope.org>
+ * Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009 Peter Baumann /
+ rasdaman GmbH.
+ *
+ * For more information please see <http://www.rasdaman.org>
  * or contact Peter Baumann via <baumann@rasdaman.com>.
- *
- * Copyright 2009 Jacobs University Bremen, Peter Baumann.
  */
 package petascope.wcst.transaction;
 
